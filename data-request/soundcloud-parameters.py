e21ec1d2847eee1e013b6c205153fc05fa0fa34d
Devin Wang <devinwang1996@gmail.com>
2021-05-09 13:30:01 -0400

    added scrolling, adjusted scrapper class

--- data-request/soundcloud-parameters.py
+++ data-request/soundcloud-parameters.py
@@ -1,53 +1,45 @@
-from bs4 import BeautifulSoup
-from bs4 import SoupStrainer
-import requests
-import urllib.request
-import os
 import pandas as pd
 import re
 import datetime
-from selenium import webdriver
 import time
+
+import requests
+import urllib.request
+from bs4 import BeautifulSoup
+from bs4 import SoupStrainer
+
+from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
 urllink = "https://soundcloud.com/charts/top?genre=all-music&country=US"
 
-# driver_path = "C:\Program Files (x86)\chromedriver.exe"
-# browser = webdriver.Chrome(executable_path=driver_path)
-# browser.get(urllink)
+driver_path = "C:\Program Files (x86)\chromedriver.exe"
+browser = webdriver.Chrome(executable_path=driver_path)
+browser.get(urllink)
+# WebDriverWait(browser, 2).until(
+#     EC.visibility_of_element_located((By.CSS_SELECTOR, "ul.lazyLoadingList__list"))
+# )
 
+last_height = browser.execute_script("return document.body.scrollHeight")
+print(last_height)
 
-def url_splitter(url):
-    urlParams = url.split("https://soundcloud.com/charts/")[1]
-    chartType = urlParams.split("?")[0]
-    genreCountry = urlParams.split("top?genre=")[1]
-    genre = genreCountry.split("&")[0]
-    restParams = genreCountry.split("&")[1]
-    country = restParams.split("=")[1]
-    return chartType, genre, country
+while True:
+    browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    time.sleep(1)
+    new_height = browser.execute_script("return document.body.scrollHeight")
+    if new_height == last_height:
+        break
+    last_height = new_height
 
+# sourceData = browser.page_source
+data = browser.find_elements_by_class_name("sc-visuallyhidden")[1:100]
 
-content = urllib.request.urlopen(urllink).read()
-h2Tags = SoupStrainer("article")
-soup = BeautifulSoup(content, "html.parser", parse_only=h2Tags).select("h2 > a")
+for element in data:
+    print(element.text)
 
-song_url_list = []
-song_name_list = []
-artist_url_list = []
-artist_name_list = []
-i = 0
-for songArtist in soup:
-    if i == 0:
-        column_name = "song"
-        song_url_list.append(str(songArtist.get("herf", "/")))
-        song_name_list.append(str(songArtist.text))
-        i += 1
-    else:
-        column_name = "artist"
-        artist_url_list.append(str(songArtist.get("href", "/")))
-        artist_name_list.append(str(songArtist.text))
-        i -= 1
-
-print(song_name_list, artist_name_list)
+browser.quit()
 
 genres = [
     "alternativerock",
--- data-request/soundcloud-scrapper.py
+++ data-request/soundcloud-scrapper.py
@@ -1,3 +1,63 @@
+from bs4 import BeautifulSoup
+from bs4 import SoupStrainer
+import requests
+import urllib.request
+import os
+import pandas as pd
+import re
+import datetime
+from selenium import webdriver
+import time
+
+
 class Scrapper:
-    def __init__(self, genres):
-        self.genres = genres
+    def __init__(self, newOrTop="top", genre="all-music", country="US"):
+        defaultURL = "https://soundcloud.com/charts/"
+        self.currentURL = (
+            defaultURL + newOrTop + "?genre=" + genre + "&country=" + country
+        )
+
+    def url_splitter(self):
+        urlParams = self.currentURL.split("https://soundcloud.com/charts/")[1]
+        chartType = urlParams.split("?")[0]
+        genreCountry = urlParams.split("top?genre=")[1]
+        genre = genreCountry.split("&")[0]
+        restParams = genreCountry.split("&")[1]
+        country = restParams.split("=")[1]
+        return chartType, genre, country
+
+    def songArtistPubCollection(self):
+        chartType, genre, country = self.url_splitter()
+
+        content = urllib.request.urlopen(self.currentURL).read()
+        h2Tags = SoupStrainer("article")
+        soupSongArtist = BeautifulSoup(
+            content, "html.parser", parse_only=h2Tags
+        ).select("h2 > a")
+
+        song_url_list = []
+        song_name_list = []
+        artist_url_list = []
+        artist_name_list = []
+        publish_date_list = []
+
+        i = 0
+        for songArtist in soupSongArtist:
+            if i == 0:
+                column_name = "song"
+                song_url_list.append(str(songArtist.get("herf", "/")))
+                song_name_list.append(str(songArtist.text))
+                i += 1
+            else:
+                column_name = "artist"
+                artist_url_list.append(str(songArtist.get("href", "/")))
+                artist_name_list.append(str(songArtist.text))
+                i -= 1
+
+        timeTags = SoupStrainer("time")
+        soupPubDate = BeautifulSoup(content, "html.parser", parse_only=timeTags)
+
+        for pubDate in soupPubDate:
+            publish_date_list.append(str(pubDate.text)[:10])
+
+        print(publish_date_list)
