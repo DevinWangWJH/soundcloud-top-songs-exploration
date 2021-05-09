8d8e7b3d71158c9b27116a5a471886d522ae1293
Devin Wang <devinwang1996@gmail.com>
2021-05-09 14:24:49 -0400

    removed items into scrapper.py

--- data-request/soundcloud-parameters.py
+++ data-request/soundcloud-parameters.py
@@ -13,33 +13,6 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
-urllink = "https://soundcloud.com/charts/top?genre=all-music&country=US"
-
-driver_path = "C:\Program Files (x86)\chromedriver.exe"
-browser = webdriver.Chrome(executable_path=driver_path)
-browser.get(urllink)
-# WebDriverWait(browser, 2).until(
-#     EC.visibility_of_element_located((By.CSS_SELECTOR, "ul.lazyLoadingList__list"))
-# )
-
-last_height = browser.execute_script("return document.body.scrollHeight")
-
-while True:
-    browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    time.sleep(1)
-    new_height = browser.execute_script("return document.body.scrollHeight")
-    if new_height == last_height:
-        break
-    last_height = new_height
-
-weeklyPlaysPath = "//span[@class='sc-visuallyhidden']"
-
-data = browser.find_elements_by_xpath(weeklyPlaysPath)[0:100]
-
-for element in data:
-    print(element.text)
-
-
 genres = [
     "alternativerock",
     "ambient",
